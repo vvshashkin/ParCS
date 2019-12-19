13de77895ad05d1384db4d9ca4b5a8fe573a75d2
GGoyman <gordeygoyman@gmail.com>
2019-12-20 00:39:38 +0300

    1)abstract outputer 2)master process ouputer 3)test

---
+++ src/ouputer_factory_mod.f90
@@ -0,0 +1,21 @@
+module outputer_factory_mod
+
+implicit none
+
+contains
+
+function create_master_process_outputer(master_id, gather_exch) result(master_process_outputer)
+
+    use master_process_outputer_mod, only : master_process_outputer_t
+    use exchange_mod,                only : exchange_t
+
+    integer(kind=4),                 intent(in) :: master_id
+    type(exchange_t),                intent(in) :: gather_exch
+    type(master_process_outputer_t)             :: master_process_outputer
+
+    master_process_outputer%master_id   = master_id
+    master_process_outputer%gather_exch = gather_exch
+
+end function create_master_process_outputer
+
+end module outputer_factory_mod
