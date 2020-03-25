8bac69990d75ccb54226da380cefd7ae2fcd5498
Vladimir Shashkin <vvshashkin@gmail.com>
2020-03-25 17:43:45 +0300

    Reformulation of stvec_abstract_mod: copy & add procedures are deferred and thus obligatory to implement in stvec classes. Smult, dot and norm procedures have avost("Not implemented") realizations in abstract class, thus can be omitted in child classes if they are not needed

--- src/stvec_abstract_mod.f90
+++ src/stvec_abstract_mod.f90
@@ -8,9 +8,11 @@ type, abstract, public, extends(state_abstract_t) :: stvec_abstract_t
 
 contains
 
-    procedure(add),  deferred :: add
-    procedure(copy), deferred :: copy
-    procedure(dot),  deferred :: dot
+    procedure(add),   deferred :: add
+    procedure(copy),  deferred :: copy
+    procedure                  :: dot
+    procedure                  :: norm
+    procedure                  :: smult
 
 end type stvec_abstract_t
 
@@ -29,16 +31,32 @@ abstract interface
         class(stvec_abstract_t), intent(inout) :: this
         class(stvec_abstract_t), intent(in)    :: source_stvec
     end subroutine copy
-
-    real(kind=8) function dot(this,other) result(dot_prod)
-        !calculates dot(this,other)
-        import stvec_abstract_t
-        class(stvec_abstract_t), intent(in) :: this
-        class(stvec_abstract_t), intent(in)    :: other
-    end function dot
-
 end interface
 
 contains
+
+real(kind=8) function norm(this) result(l2)
+    class(stvec_abstract_t), intent(in) :: this
+    !calculates norm of state vector
+    l2 = 0._8
+    call avost("norm function not implemented for specific stvec class")
+end function norm
+
+subroutine smult(this, alpha)
+    !multiplicates state vector by scalar
+    class(stvec_abstract_t), intent(inout) :: this
+    real(kind=8),            intent(in)    :: alpha
+
+    call avost("scalar multiplication not implemented for specific stvec class")
+end subroutine smult
+
+real(kind=8) function dot(this,other) result(dot_prod)
+    !calculates dot(this,other)
+    class(stvec_abstract_t), intent(in) :: this
+    class(stvec_abstract_t), intent(in) :: other
+
+    dot_prod = 0._8
+    call avost("dot product function is not implemented for specific stvec class")
+end function dot
 
 end module stvec_abstract_mod
