fef592144f00196ee377012872e9801f502ea705
gordey <gordey@linux-k67v.site>
2021-05-08 14:28:29 +0300

    exch v0

--- src/parallel/exchange_factory_mod.f90
+++ src/parallel/exchange_factory_mod.f90
@@ -1,12 +1,160 @@
 module exchange_factory_mod
 
-use partition_mod,        only : partition_t
-use tile_mod,             only : tile_t
+use parcomm_mod,   only : parcomm_t
+use partition_mod, only : partition_t
+use tile_mod,      only : tile_t
 
 implicit none
 
 contains
+function create_symm_halo_exchange(partition, parcomm, halo_width, halo_type) result(exchange)
 
+    use exchange_halo_mod,    only : exchange_2D_halo_t
+
+    type(partition_t), target, intent(in)    :: partition
+
+    integer(kind=4), intent(in) :: halo_width
+    character(*),    intent(in) :: halo_type
+    type(parcomm_t), intent(in) :: parcomm
+
+    type(exchange_2D_halo_t), allocatable :: exchange
+
+    type(tile_t), pointer :: local_tile, remote_tile
+
+    integer(kind=4),  dimension((partition%num_panels*partition%num_tiles)**2) :: &
+                      recv_is, recv_ie, recv_js, recv_je, recv_ks, recv_ke, &
+                      send_is, send_ie, send_js, send_je, send_ks, send_ke
+    integer(kind=4),  dimension((partition%num_panels*partition%num_tiles)**2) :: &
+                      send_to_proc_id, recv_from_proc_id   , &
+                      recv_to_tile_ind , &
+                      send_from_tile_ind, &
+                      send_tag, recv_tag
+    integer(kind=4),  dimension((partition%num_panels*partition%num_tiles)**2) :: &
+                      send_pts_num, recv_pts_num
+    integer(kind=4),  dimension((partition%num_panels*partition%num_tiles)**2) :: &
+                      send_i_step, send_j_step
+    character(len=1), dimension((partition%num_panels*partition%num_tiles)**2) :: &
+                      first_dim_index
+
+
+    integer(kind=4) :: local_ind, remote_ind, exch_num, ind,ts ,te
+    logical :: is_intersection
+
+    if (halo_width > partition%Nh) then
+        write(*,*) 'Error! Halo is too wide! Abort!'
+        stop
+    end if
+
+    ts = partition%ts
+    te = partition%te
+
+    exch_num = 0
+
+    do local_ind = 1, partition%num_panels*partition%num_tiles
+
+        if (partition%proc_map(local_ind) /= myid) cycle
+
+        local_tile => partition%tile(local_ind)
+
+        do remote_ind = 1, partition%num_panels*partition%num_tiles
+
+            remote_tile => partition%tile(remote_ind)
+
+            select case (halo_type)
+
+            case('cross')
+
+            call find_cross_halo_intersection(local_tile, remote_tile, halo_width, partition%Nh, is_intersection , &
+                                              recv_is(exch_num + 1), recv_ie(exch_num + 1)                            , &
+                                              recv_js(exch_num + 1), recv_je(exch_num + 1)                            , &
+                                              send_is(exch_num + 1), send_ie(exch_num + 1)                            , &
+                                              send_js(exch_num + 1), send_je(exch_num + 1)                            , &
+                                              send_i_step(exch_num+1), send_j_step(exch_num+1), first_dim_index(exch_num+1) )
+
+            case('full')
+                call find_full_halo_intersection(local_tile, remote_tile, halo_width, partition%Nh, is_intersection , &
+                                                  recv_is(exch_num + 1), recv_ie(exch_num + 1)                            , &
+                                                  recv_js(exch_num + 1), recv_je(exch_num + 1)                            , &
+                                                  send_is(exch_num + 1), send_ie(exch_num + 1)                            , &
+                                                  send_js(exch_num + 1), send_je(exch_num + 1)                            , &
+                                                  send_i_step(exch_num+1), send_j_step(exch_num+1), first_dim_index(exch_num+1) )
+            case default
+                call avost("Wrong halo_type in create_Agrid_halo_exchange function! Stop!")
+            end select
+
+            if (is_intersection) then
+                exch_num = exch_num + 1
+
+                recv_to_tile_ind  (exch_num) = local_ind
+
+                send_from_tile_ind(exch_num) = local_ind
+
+                send_ks(exch_num) = local_tile%ks
+                send_ke(exch_num) = local_tile%ke
+
+                recv_ks(exch_num) = remote_tile%ks
+                recv_ke(exch_num) = remote_tile%ke
+
+                send_pts_num(exch_num) = (send_ie(exch_num) - send_is(exch_num) + 1)* &
+                                         (send_je(exch_num) - send_js(exch_num) + 1)* &
+                                         (send_ke(exch_num) - send_ks(exch_num) + 1)
+
+                recv_pts_num(exch_num) = (recv_ie(exch_num) - recv_is(exch_num) + 1)* &
+                                         (recv_je(exch_num) - recv_js(exch_num) + 1)* &
+                                         (recv_ke(exch_num) - recv_ks(exch_num) + 1)
+
+                send_to_proc_id  (exch_num) = partition%proc_map(remote_ind)
+                recv_from_proc_id(exch_num) = partition%proc_map(remote_ind)
+
+                send_tag(exch_num) = 6*partition%num_tiles*(local_ind-1) + remote_ind
+                recv_tag(exch_num) = 6*partition%num_tiles*(remote_ind-1) + local_ind
+
+            end if
+
+        end do
+
+    end do
+
+    allocate(exchange, source = exchange_2D_halo_t(              &
+            send_is            = send_is(1:exch_num),          &
+            send_ie            = send_ie(1:exch_num),          &
+            send_js            = send_js(1:exch_num),          &
+            send_je            = send_je(1:exch_num),          &
+            send_ks            = send_ks(1:exch_num),          &
+            send_ke            = send_ke(1:exch_num),          &
+            recv_is            = recv_is(1:exch_num),          &
+            recv_ie            = recv_ie(1:exch_num),          &
+            recv_js            = recv_js(1:exch_num),          &
+            recv_je            = recv_je(1:exch_num),          &
+            recv_ks            = recv_ks(1:exch_num),          &
+            recv_ke            = recv_ke(1:exch_num),          &
+            send_i_step        = send_i_step(1:exch_num),      &
+            send_j_step        = send_j_step(1:exch_num),      &
+            first_dim_index    = first_dim_index(1:exch_num),  &
+            recv_to_tile_ind   = recv_to_tile_ind(1:exch_num),    &
+            send_from_tile_ind = send_from_tile_ind(1:exch_num),    &
+            send_to_proc_id    = send_to_proc_id(1:exch_num),     &
+            recv_from_proc_id  = recv_from_proc_id(1:exch_num),     &
+            send_points_num    = send_pts_num(1:exch_num),     &
+            recv_points_num    = recv_pts_num(1:exch_num),     &
+            send_tag           = send_tag(1:exch_num),         &
+            recv_tag           = recv_tag(1:exch_num),         &
+            send_number        = exch_num                ,     &
+            recv_number        = exch_num, &
+            ts                 = ts, &
+            te                 = te) )
+
+    ! call exchange%profile%check()
+    !
+     allocate(exchange%send_buff(exch_num)   , exchange%recv_buff(exch_num)   )
+     allocate(exchange%mpi_send_req(exch_num), exchange%mpi_recv_req(exch_num))
+    do ind = 1, exch_num
+        call exchange%send_buff(ind)%init(send_pts_num(ind))
+        call exchange%recv_buff(ind)%init(recv_pts_num(ind))
+    end do
+
+
+end function create_symm_halo_exchange
 function create_Agrid_halo_exchange(partition, halo_width, halo_type, myid, np ) result(exchange)
 
     use exchange_halo_mod,    only : exchange_2D_halo_t
--- src/topology_mod.f90
+++ src/topology_mod.f90
@@ -174,5 +174,72 @@ subroutine transform_index(pn_out, pn_in, Npoints, i_in, j_in, i_out, j_out, i_s
     end if
 
 end subroutine transform_index
+subroutine transform_index_new(pn_in, i_in, j_in, nx, ny, pn_out, i_out, j_out, i_step, j_step, first_dim_index)
 
+    integer(kind=4),  intent(in)            :: pn_out, pn_in, nx, ny, i_in, j_in
+    integer(kind=4),  intent(out)           :: i_out , j_out
+    integer(kind=4),  intent(out), optional :: i_step, j_step  !Determines descending or ascending order of loop
+    character(len=1), intent(out), optional :: first_dim_index !Determines first loop index in horizontal direction
+
+    integer(kind=4) :: ex_local(1:3), ey_local(1:3), r_local(1:3), shift(2)
+    integer(kind=4) :: ex_ex, ex_ey, ex_r, ey_ex, ey_ey, ey_r
+    logical :: is_opposite, is_right, is_left, is_bottom, is_top
+
+    is_right    = (dot_product(ex(1:3,pn_out), n(1:3,pn_in)) == -1)
+    is_left     = (dot_product(ex(1:3,pn_out), n(1:3,pn_in)) ==  1)
+    is_bottom   = (dot_product(ey(1:3,pn_out), n(1:3,pn_in)) ==  1)
+    is_top      = (dot_product(ey(1:3,pn_out), n(1:3,pn_in)) == -1)
+    is_opposite = (dot_product( n(1:3,pn_out), n(1:3,pn_in)) == -1)
+
+    if (is_right) then
+        ex_local = n (1:3, pn_out)
+        ey_local = ey(1:3, pn_out)
+        r_local  = r (1:3, pn_out) + ex(1:3, pn_out)
+        shift = [nx,0]
+    else if (is_left) then
+        ex_local = -n (1:3, pn_out)
+        ey_local =  ey(1:3, pn_out)
+        r_local  =  r (1:3, pn_out) - ex_local(1:3)
+        shift = [-nx,0]
+    else if (is_bottom) then
+        ex_local =  ex(1:3, pn_out)
+        ey_local = -n(1:3, pn_out)
+        r_local  =  r (1:3, pn_out) + n(1:3, pn_out)
+        shift = [0,-ny]
+    else if (is_top) then
+        ex_local =  ex(1:3, pn_out)
+        ey_local =  n(1:3, pn_out)
+        r_local  =  r(1:3, pn_out) +ey(1:3,pn_out)
+        shift = [0, ny]
+    else if (is_opposite) then
+        ex_local = -ex(1:3, pn_out)
+        ey_local =  ey(1:3, pn_out)
+        r_local  =  r (1:3, pn_out) + ex(1:3, pn_out) + n(1:3, pn_out)
+        shift = [2*nx,0]
+    end if
+
+    ex_ex = dot_product(ex(1:3, pn_in), ex_local(1:3))
+    ex_ey = dot_product(ey(1:3, pn_in), ex_local(1:3))
+    ex_r  = dot_product(r(1:3, pn_in) - r_local(1:3), ex_local(1:3))
+
+    ey_ex = dot_product(ex(1:3, pn_in), ey_local(1:3))
+    ey_ey = dot_product(ey(1:3, pn_in), ey_local(1:3))
+    ey_r  = dot_product(r(1:3, pn_in) - r_local(1:3), ey_local(1:3))
+
+    i_out = (i_in-1)*ex_ex + (j_in-1)*ex_ey + (nx-1)*ex_r + 1 + shift(1)
+    j_out = (i_in-1)*ey_ex + (j_in-1)*ey_ey + (ny-1)*ey_r + 1 + shift(2)
+
+    if (present(i_step).and.present(j_step).and. present(first_dim_index) ) then
+        if ( ex_ex /= 0 ) then
+            first_dim_index = 'i'
+            i_step = ex_ex
+            j_step = ey_ey
+        else
+            first_dim_index = 'j'
+            i_step = ex_ey
+            j_step = ey_ex
+        end if
+    end if
+
+end subroutine transform_index_new
 end module topology_mod
