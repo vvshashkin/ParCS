2e6151a4d9c12e97ae6068be5f3ec9cfbc6c436e
Vladimir Shashkin <vvshashkin@gmail.com>
2021-08-16 19:56:36 +0300

    div/grad abstract classes

--- src/differential_operators/abstract_div_mod.f90
+++ src/differential_operators/abstract_div_mod.f90
@@ -1,8 +1,7 @@
 module abstract_div_mod
 
 use grid_function_mod, only : grid_function_t
-use partition_mod,     only : partition_t
-use mesh_mod,          only : mesh_t
+use domain_mod,        only : domain_t
 
 implicit none
 
@@ -15,15 +14,14 @@ procedure(div_calc_procedure), deferred :: calc_div
 end type div_operator_t
 
 abstract interface
-    subroutine div_calc_procedure(this, u, v, div, mesh, partition, multiplier)
-        import div_operator_t, grid_function_t, mesh_t, partition_t
+    subroutine div_calc_procedure(this, div, u, v, domain, multiplier)
+        import div_operator_t, grid_function_t, domain_t
         class(div_operator_t),  intent(inout) :: this
-        type(partition_t),      intent(in)    :: partition
-        type(grid_function_t),  intent(inout) ::   u(partition%ts:partition%te), &
-                                                   v(partition%ts:partition%te), &
-                                                 div(partition%ts:partition%te)
-        type(mesh_t),           intent(in)    :: mesh(partition%ts:partition%te)
+        type(domain_t),         intent(in)    :: domain
+        type(grid_function_t),  intent(inout) :: u, v
         real(kind=8), optional, intent(in)    :: multiplier
+        !out put
+        type(grid_function_t),  intent(inout) :: div
     end subroutine div_calc_procedure
 end interface
 
--- src/differential_operators/abstract_grad_mod.f90
+++ src/differential_operators/abstract_grad_mod.f90
@@ -1,29 +1,30 @@
 module abstract_grad_mod
 
 use grid_function_mod, only : grid_function_t
-use partition_mod,     only : partition_t
-use mesh_mod,          only : mesh_t
-
+use domain_mod,        only : domain_t
+    
 implicit none
-
+    
 type, abstract, public :: grad_operator_t
+    
 contains
-    procedure(grad_calc_procedure), deferred :: calc_grad
+    
+procedure(grad_calc_procedure), deferred :: grad_div
+    
 end type grad_operator_t
-
+    
 abstract interface
-    subroutine grad_calc_procedure(this, f, gx, gy, mesh, partition, multiplier)
-        import grad_operator_t, grid_function_t, mesh_t, partition_t
-        class(grad_operator_t), intent(inout) :: this
-        type(partition_t),      intent(in)    :: partition
-        type(grid_function_t),  intent(inout) ::  f(partition%ts:partition%te), &
-                                                 gx(partition%ts:partition%te), &
-                                                 gy(partition%ts:partition%te)
-        type(mesh_t),           intent(in)    :: mesh(partition%ts:partition%te)
+    subroutine grad_calc_procedure(this, gx, gy, f, domain, multiplier)
+        import div_operator_t, grid_function_t, domain_t
+        class(div_operator_t),  intent(inout) :: this
+        type(domain_t),         intent(in)    :: domain
+        type(grid_function_t),  intent(inout) :: f
         real(kind=8), optional, intent(in)    :: multiplier
+        !output:
+        type(grid_function_t),  intent(inout) :: gx, gy
     end subroutine grad_calc_procedure
 end interface
-
+    
     contains
-
+   
 end module abstract_grad_mod
