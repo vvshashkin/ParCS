c17fa0fbf8a6976bef5531e358bd691e5745caaf
Vladimir Shashkin <vvshashkin@gmail.com>
2021-08-21 13:35:35 +0300

    ifixed polar bug in latlon_functions: sin_lam, cos_lam

--- src/test_fields/latlon_functions_mod.f90
+++ src/test_fields/latlon_functions_mod.f90
@@ -16,8 +16,11 @@ contains
         c = x / max(sqrt(x**2+y**2),1e-14)
     end function cos_lam
 
-    real(kind=8) pure function sin_lam(x,y,z) result(c)
+    real(kind=8) pure function sin_lam(x,y,z) result(s)
         real(kind=8), intent(in) :: x,y,z
-        c = y / max(sqrt(x**2+y**2),1e-14)
+        !s = cos_lam(x,y,z)
+        !s = sqrt(1.0_8-s**2)
+        s = y / max(sqrt(x**2+y**2),1e-14)
+        if(x**2+y**2 == 0) s = 1.0_8
     end function sin_lam
 end module latlon_functions_mod
